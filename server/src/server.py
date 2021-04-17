42a7011a501f2e8ca39f5e601d54eb13f9a27f1d
Heisenberg <nancologist@gmail.com>
2021-04-17 12:18:13 +0200

    Create a fake db.

--- server/src/server.py
+++ server/src/server.py
@@ -1,7 +1,21 @@
 from sanic import Sanic
 from sanic.response import json
 
-
+# Fake DB =========================
+data = [
+    # status:
+    {
+        "id": "js32d",
+        "author": "John",
+        "text": 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren'
+    },
+    {
+        "id": 'xmk32',
+        "author": 'Sarah',
+        "text": "no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
+    }
+]
+# =================================
 
 app = Sanic("app_server_2021")
 
@@ -9,10 +23,10 @@ app = Sanic("app_server_2021")
 async def index(request):
     return json({"hello": "world"})
 
-@app.post('/test')
-async def get_test(req):
-    print(req.json)
-    return json({"msg": "ok"})
+@app.route('/statuses')
+async def index(request):
+    return json(data, indent=2)
+
 
 if __name__ == "__main__":
-    app.run() 
+    app.run()
