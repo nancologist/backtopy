e78c1f9361592d207e42701a5f57e0a0753b650b
Heisenberg <nancologist@gmail.com>
2021-04-30 00:42:08 +0200

    Add Nav Style

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -4,7 +4,7 @@
   <nav class="nav">
     <ul class="nav__list">
       <li class="nav__list__item">
-        <router-link to="/">Home</router-link>
+        <router-link exact to="/">Home</router-link>
       </li>
       <li class="nav__list__item">
         <router-link to="/about">About</router-link>
@@ -18,5 +18,32 @@
 </template>
 
 <style scoped>
+.header {
+  background-color: #333;
+  box-shadow: 1px 2px #ccc;
+  color: #fff;
+  padding: 10px;
+}
 
+.nav__list {
+  color: #ccc;
+  display: flex;
+  justify-content: space-evenly;
+  margin: 20px auto;
+  width: 60%;
+}
+
+.nav__list__item {
+  border-bottom: 2px solid transparent;
+  padding: 2px;
+}
+
+.nav__list__item:hover {
+  border-bottom: 2px solid white;
+}
+
+.router-link-active {
+  color: white;
+  font-weight: 600;
+}
 </style>
